a942e53111eef313874b68cfe23dcc007996eef2
Takis Pournaras <takis.pournaras@gmail.com>
2023-09-01 21:13:19 +0300

    better one hand

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,8 +2,10 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    fontFamily: {
-      sans: ["Inter", "ui-sans-serif", "system-ui"],
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "ui-sans-serif", "system-ui"],
+      },
     },
   },
   plugins: [require("@tailwindcss/typography")],
